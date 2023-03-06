31264d5a3f3b8e4e2d7e4c3c69842669916b83e3
Lily Gostovic <lily.gostovic@gmail.com>
2023-03-06 10:42:26 +0100

    Add basic content to Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-export const Home = () => {
-  const x = 10;
-
-  return <text>{x}</text>;
-};
+export const Home = () => (
+  <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+    <text>Music Sport Performance Analysis</text>
+    <text>
+      Once developed, you will be able to log in to your Strava and Spotify profiles to see what
+      music makes you perform the best!
+    </text>
+  </div>
+);
